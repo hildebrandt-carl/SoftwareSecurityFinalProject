6fca59e8a39546c4882ddfa726882c843c3bdf90
Carl Hildebrandt <hildebrandt.carl@outlook.com>
2018-11-28 17:52:19 -0500

    Removed redundant code

--- Code/code_ws/build/drive_to_goal/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/carldesktop/Documents/SoftwareSecurityFinalProject/Code/code_ws/src/drive_to_goal
-# Build directory: /home/carldesktop/Documents/SoftwareSecurityFinalProject/Code/code_ws/build/drive_to_goal
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
